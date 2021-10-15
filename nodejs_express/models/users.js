50df5a504be8bf1c620086f536c9468505e1938b
alicanbaydar <alican.baydar94@gmail.com>
2021-10-15 23:30:26 +0300

    bcrypt-password
    bcrypt-password

--- nodejs_express/models/users.js
+++ nodejs_express/models/users.js
@@ -1,17 +1,24 @@
+const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
     username: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
-        required: true,
-        unique: true
+        required: true
     }
 }, {timestamps: true})
+
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
 
 const User = mongoose.model('User',userSchema)
 module.exports = User
