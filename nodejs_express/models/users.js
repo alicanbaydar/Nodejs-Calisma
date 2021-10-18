faf5a3f9121238470e3fba3fecacf1aaef7f7448
alicanbaydar <alican.baydar94@gmail.com>
2021-10-18 23:51:48 +0300

    jwt intro

--- nodejs_express/models/users.js
+++ nodejs_express/models/users.js
@@ -14,6 +14,22 @@ const userSchema = new Schema({
     }
 }, {timestamps: true})
 
+
+userSchema.statics.login = async function(username,password) {
+    const user = await this.findOne({username})
+    if (user) {
+        const auth = await bcrypt.compare(password,user.password)
+        if(auth) {
+            return user
+        }else{
+            throw Error('parola hatali')
+        }
+    }else{
+        throw Error('kullanici yok')
+    }
+}
+
+
 userSchema.pre('save', async function (next) {
     const salt = await bcrypt.genSalt()
     this.password = await bcrypt.hash(this.password, salt)
